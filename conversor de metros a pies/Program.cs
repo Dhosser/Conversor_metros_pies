04b3763b4601969d982c8d977c73e54635e10428
Mr.Sas <80982769+Dhosser@users.noreply.github.com>
2024-02-29 16:03:51 -0300

    Add files via upload

---
+++ conversor de metros a pies/Program.cs
@@ -0,0 +1,18 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        int num;
+
+        Console.WriteLine("Longitud de metros:");
+        num = int.Parse(Console.ReadLine());
+        float resultado = num * 3.281f;
+
+        if (num > 0)
+        {
+            Console.WriteLine(num + " " + "metros son" + " " + resultado + " " + "pies");
+        }
+    }
+
+}
